547ec49e7d8a19697249f7de182e271140cea1ab
Daniel Lupascu <liviudaniel.lupascu@gmail.com>
2019-03-09 08:16:06 +0200

    Initial setup 3h

---
+++ Turns/Assets/Scripts/FloorManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    public Transform tilesHolder;
+    public float initialSpeed;
+    public int initalTiles = 6;
+
+    Stack<FloorTile> tiles;
+    Vector3 nextTilePosition = new Vector3(0, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void InitialSetup()
+    {
+        tiles = new Stack<FloorTile>();
+
+        for (int i = 0; i < initalTiles; i++)
+        {
+            var tile = PoolManager.Instance.TilesPool.Spawn(nextTilePosition, Quaternion.identity, tilesHolder)
+                .GetComponent<FloorTile>();
+            tiles.Append(tile);
+            
+            SetNextPosition(Vector3.forward);
+        }
+    }
+
+    public void SetNextPosition(Vector3 direction)
+    {
+        nextTilePosition += direction;
+    }
+}
---
+++ Turns/Assets/Scripts/PoolManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Gamelogic.Extensions;
+using Lean.Pool;
+using UnityEngine;
+
+public class PoolManager : Singleton<PoolManager>
+{
+    public LeanGameObjectPool TilesPool;
+}
